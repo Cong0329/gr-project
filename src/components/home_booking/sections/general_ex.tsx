7ac273df31e79cd61ef63f13ede217f861e33c22
PQNIST1 <nguyenpham23476@gmail.com>
2024-10-03 13:01:03 +0700

    fix import sale

--- src/components/home_booking/sections/general_ex.tsx
+++ src/components/home_booking/sections/general_ex.tsx
@@ -3,6 +3,7 @@ import img from "../../../assets/logo.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FaStar } from "react-icons/fa";
 
 
 
